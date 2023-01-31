ebf6f392eb0eccc4af78efa6337175f071da12fb
Green <97376220+Nga2710@users.noreply.github.com>
2023-02-01 06:23:05 +0700

    Add files via upload

--- 3xplus1.py
+++ 3xplus1.py
@@ -5,16 +5,18 @@ def clrscr():
 while True:
   try:
     x = int(input("x = "))
-    if x > 0:
-      break
+    break
   except ValueError:
     print("ValueError, try again!")
 clrscr()
-result = []
-while x != 1:
-  result.append(str(int(x)))
+result = [str(int(x))]
+while True:
   if x % 2 == 0:
     x = x / 2
   else:
     x = 3*x + 1
+  if str(int(x)) in result:
+    break
+  else:
+    result.append(str(int(x)))
 print("\x20".join(result))
